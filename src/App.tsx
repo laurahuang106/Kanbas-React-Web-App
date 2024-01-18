ed343537269415254e8414029e5c342cb50f096f
laurahuang106 <huangyuying2018@hotmail.com>
2024-01-17 22:08:25 -0800

    udpate home page and lab-a1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Welcome to Web Dev CS5610 - 07 Spring 2024</h1>
+      <ul>
+        <li>
+          <a href="labs/a1/index.html">Assignment 1</a>
+        </li>
+        <li>
+          <a href="labs/a2/index.html">Assignment 2</a>
+        </li>
+        <li>
+          <a href="/Kanbas/Courses/Home/screen.html">Kanbas Course Home</a>
+        </li>
+      </ul>
+    </>
   );
 }
 
