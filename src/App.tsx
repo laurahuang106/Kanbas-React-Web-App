c09d6ddfc5fe67cf2fab7c27e269b901e990a9fb
laurahuang106 <huangyuying2018@hotmail.com>
2024-02-03 11:39:50 -0800

    4.4 mini screen for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,26 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 function App() {
   return (
-    <>
-      <h1>Welcome to Web Dev CS5610 - 02 Spring 2024</h1>
-      <ul>
-        <li>
-          <a href="labs/a1/index.html">Assignment 1</a>
-        </li>
-        <li>
-          <a href="labs/a2/index.html">Assignment 2</a>
-        </li>
-        <li>
-          <a href="/Kanbas/Courses/Home/screen.html">Kanbas Course Home</a>
-        </li>
-      </ul>
-    </>
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <img src={logo} className="App-logo" alt="logo" />
+    //         <p>
+    //           Edit <code>src/App.tsx</code> and save to reload.
+    //         </p>
+    //         <a
+    //           className="App-link"
+    //           href="https://reactjs.org"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //         >
+    //           Learn React
+    //         </a>
+    //       </header>
+    //     </div>
+
+    <></>
   );
 }
 
