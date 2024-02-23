4de0ba91d5b0ce7bf594e849e8d8e480df39470c
laurahuang106 <huangyuying2018@hotmail.com>
2024-02-23 10:11:40 -0800

    lab4 up to section2.3

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,7 +1,33 @@
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
+
 function Assignment4() {
+  function sayHello() {
+    alert("Hello");
+  }
   return (
     <div>
       <h2>Assignment 4</h2>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
     </div>
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -9,7 +9,7 @@ function Labs() {
       <h1>Labs</h1>
       <Nav />
       <Link to="/Labs/a3">Assignment 3</Link> |
-      <Link to="/Labs/a4">Assignment 4</Link>
+      <Link to="/Labs/a4"> Assignment 4</Link>
       <Routes>
         <Route path="/a3/*" element={<Assignment3 />} />
         <Route path="/a4" element={<Assignment4 />} />
