37bc09ec6c8e7ff71b1130705fc11356c30907ae
laurahuang106 <huangyuying2018@hotmail.com>
2024-03-26 14:29:02 -0700

    lab5 3.4.4

--- src/Labs/a5/EncodingParametersInURLs.tsx
+++ src/Labs/a5/EncodingParametersInURLs.tsx
@@ -1,11 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function EncodingParametersInURLs() {
   const [a, setA] = useState(34);
   const [b, setB] = useState(23);
+  const [welcome, setWelcome] = useState("");
+  const [result, setResult] = useState(0);
+  const fetchWelcome = async () => {
+    const response = await axios.get("http://localhost:4000/a5/welcome");
+    setWelcome(response.data);
+  };
+  const fetchSum = async (a: number, b: number) => {
+    const response = await axios.get(`http://localhost:4000/a5/add/${a}/${b}`);
+    setResult(response.data);
+  };
+  const fetchSubtraction = async (a: number, b: number) => {
+    const response = await axios.get(
+      `http://localhost:4000/a5/subtract/${a}/${b}`
+    );
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    fetchWelcome();
+  }, []);
+
   return (
     <div>
       <h3>Encoding Parameters In URLs</h3>
       <h4>Calculator</h4>
+      <h5>Fetching Welcome</h5>
+      <h6>{welcome}</h6>
+
       <input
         type="number"
         value={a}
@@ -16,6 +42,15 @@ function EncodingParametersInURLs() {
         onChange={(e) => setB(Number(e.target.value))}
         value={b}
       />
+      <input value={result} type="number" readOnly />
+      <h3>Fetch Result</h3>
+      <button className="btn btn-primary" onClick={() => fetchSum(a, b)}>
+        Fetch Sum of {a} + {b}
+      </button>
+      <button className="btn btn-danger" onClick={() => fetchSubtraction(a, b)}>
+        Fetch Substraction of {a} - {b}
+      </button>
+
       <h3>Path Parameters</h3>
       <a
         href={`http://localhost:4000/a5/add/${a}/${b}`}
