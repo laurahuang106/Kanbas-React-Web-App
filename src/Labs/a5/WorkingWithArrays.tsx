eb2edd3f6e8312f0965aec991ed696f98cd08dbb
laurahuang106 <huangyuying2018@hotmail.com>
2024-03-28 14:50:40 -0700

    lab 3.4

--- src/Labs/a5/WorkingWithArrays.tsx
+++ src/Labs/a5/WorkingWithArrays.tsx
@@ -1,14 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Todo {
+  id: number;
+  title: string;
+  description: string;
+  due: string;
+  completed: boolean;
+}
 
 function WorkingWithArrays() {
   const API = "http://localhost:4000/a5/todos";
-  const [todo, setTodo] = useState({
+  const [todo, setTodo] = useState<Todo>({
     id: 1,
     title: "NodeJS Assignment",
     description: "Create a NodeJS server with ExpressJS",
     due: "2021-09-09",
     completed: false,
   });
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const fetchTodos = async () => {
+    const response = await axios.get(API);
+    setTodos(response.data);
+  };
+  const removeTodo = async (todo: { id: any }) => {
+    const response = await axios.get(`${API}/${todo.id}/delete`);
+    setTodos(response.data);
+  };
+  const createTodo = async () => {
+    const response = await axios.get(`${API}/create`);
+    setTodos(response.data);
+  };
+  const fetchTodoById = async (id: any) => {
+    const response = await axios.get(`${API}/${id}`);
+    setTodo(response.data);
+  };
+  const updateTitle = async () => {
+    const response = await axios.get(`${API}/${todo.id}/title/${todo.title}`);
+    setTodos(response.data);
+  };
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   return (
     <div>
@@ -17,6 +50,29 @@ function WorkingWithArrays() {
       <a className="btn btn-primary" href={API}>
         Get Todos
       </a>
+      <button className="btn btn-primary ms-3" onClick={createTodo}>
+        Create Todo
+      </button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}
+            <button className="btn btn-danger" onClick={() => removeTodo(todo)}>
+              Remove
+            </button>
+            <button
+              className="btn btn-success ms-3"
+              onClick={() => fetchTodoById(todo.id)}
+            >
+              Edit
+            </button>
+            <button className="btn btn-primary ms-3" onClick={updateTitle}>
+              Update Title
+            </button>
+          </li>
+        ))}
+      </ul>
 
       <h4>Retrieving an Item from an Array by ID</h4>
       <input
